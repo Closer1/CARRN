1e2c1facfd621010596001ee2e482db5d3044120
tanhanlin <495415920@qq.com>
2020-05-25 13:25:50 +0800

    init

--- train.py
+++ train.py
@@ -266,3 +266,7 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
+
+
+if __name__ == '__main__':
+    main()
